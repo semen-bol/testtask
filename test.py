5550b27caf31d0f5177ee2b5b38ff232f668d067
semen-bol <syoma.bolotov@bk.ru>
2025-10-23 15:45:39 +0500

    feat(test): add check_file_exists in tests

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pytest
 import asyncio
 
-from main import read_csv_files, average_rating_report
+from main import read_csv_files, average_rating_report, check_file_exists
 
 csv_files = ['products1.csv', 'products2.csv']
 report = "average-rating"
@@ -9,6 +9,13 @@ report = "average-rating"
 class TestReportGenerator:
     @pytest.mark.asyncio
     async def test_csv_read(self):
+        for file in csv_files:
+            file_check = await check_file_exists(file)
+
+            if file_check:
+                pass
+            else:
+                raise FileNotFoundError(f"Файл {file} не найден")
         csv_data = await read_csv_files(csv_files)
         
         assert len(csv_data) > 0
@@ -16,6 +23,14 @@ class TestReportGenerator:
 
     @pytest.mark.asyncio
     async def test_generate_report(self):
+        for file in csv_files:
+            file_check = await check_file_exists(file)
+
+            if file_check:
+                pass
+            else:
+                raise FileNotFoundError(f"Файл {file} не найден")
+
         csv_data = await read_csv_files(csv_files)
         tab = await average_rating_report(csv_data)
 
